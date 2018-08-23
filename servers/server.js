5f707e0203fb759f40da9f68cbb20f2746070906
thelaceyedit <thelaceyedit@gmail.com>
2018-08-23 19:53:31 -0400

    tweaking servers

--- servers/server.js
+++ servers/server.js
@@ -1,26 +1,26 @@
 var http = require("http");
 
-var PORT = 7000;
-var PORT2 = 7500;
+var PORTONE = 7000;
+var PORTTWO = 7500;
 
-var server = http.createServer(function(request, response) {
+var serverOne = http.createServer(function(request, response) {
     console.log("this is the good server");
     response.end("You look amaze! You hit: " + request.url);
 
 });
 
-var server2 = http.createServer(function(request, response) {
+var serverTwo = http.createServer(function(request, response) {
     console.log("this is the bad server");
     response.end("Your shirt is uglay! You hit: " + response.url);
 
 });
 
-server.listen(PORT, function() {
-    console.log("server started listening on http://localhost:" + PORT);
+serverOne.listen(PORTONE, function() {
+    console.log("server started listening on http://localhost:" + PORTONE);
 
 });
 
-server2.listen(PORT2, function() {
-    console.log("server started listening on http://localhost:" + PORT2);
+serverTwo.listen(PORTTWO, function() {
+    console.log("server started listening on http://localhost:" + PORTTWO);
 
 });
